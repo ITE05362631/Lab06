d84924fd9f764d0c154cc8a567d3d446b009a03e
stonePC <ITE05362631@github.com>
2016-10-17 17:40:48 +0800

    ver2.0

--- Client/Client.c
+++ Client/Client.c
@@ -8,34 +8,48 @@ int main(int argc , char *argv[])
 {
     int socket_desc;
     struct sockaddr_in server;
-    char *message , server_reply[2000];
+    char message[2000] , server_reply[2000];
+    int port;
+
+    if (argc != 3){
+        perror("參數錯誤");
+    }
+
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     if (socket_desc == -1)
     {
         printf("Could not create socket");
     }
 
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_addr.s_addr = inet_addr(argv[1]);
     server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
+    sscanf(argv[2],"%d",&port);
+
+    server.sin_port = htons( port );
     if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
         puts("connect error");
         return 1;
     }
     puts("Connected\n");
-    message = "GET / HTTP/1.1\r\n\r\n";
-    if( send(socket_desc , message , strlen(message) , 0) < 0)
+    while (1)
     {
-        puts("Send failed");
-        return 1;
+        printf("Input : ");
+        scanf("%s", &message);
+        if (send(socket_desc, message, strlen(message), 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+        puts("Data Send：");
+        puts(message);
+        if (recv(socket_desc, server_reply, 2000, 0) < 0)
+        {
+            puts("recv failed");
+        }
+        puts("Reply received:");
+        puts(server_reply);
     }
-    puts("Data Send\n");
-    if( recv(socket_desc, server_reply , 2000 , 0) < 0)
-    {
-        puts("recv failed");
-    }
-    puts("Reply received\n");
-    puts(server_reply);
+
     return 0;
     }
--- Server/Server.c
+++ Server/Server.c
@@ -1,45 +1,54 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<time.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
 
-int main(int atgc,char *argv[]){
-    int socket_desc,new_socket,c;
-    struct sockaddr_in server , client;
-    char *message;
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if(socket_desc == -1){
+int main(int atgc, char *argv[])
+{
+    int socket_desc, new_socket, c;
+    struct sockaddr_in server, client;
+    char message[2000];
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1)
+    {
         printf("Connot create socket");
     }
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(8888);
-    if(bind(socket_desc,(struct sockaddr *)&server,sizeof(server))<0){
+    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
         puts("bind failed");
         return 1;
     }
     puts("bind done");
-    listen(socket_desc , 3);
+    listen(socket_desc, 3);
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
-    while( (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    while ((new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c)))
     {
-        if (new_socket<0)
+        if (new_socket < 0)
         {
             perror("accept failed");
             return 1;
         }
         puts("Connection accepted");
 
-        struct tm *ptr;
-        time_t lt;
-        lt=time(NULL); 
-        message = "server time : ";
-        write(new_socket , message , strlen(message));
-        message = ctime(&lt);
-        write(new_socket , message , strlen(message));
+        while (1)
+        {   
+            if (recv(new_socket, message, 2000, 0) < 0)
+            {
+                perror("recv error");
+            }
+            else
+            {
+                puts("Data Received:");
+                puts(message);
+                write(new_socket, message, strlen(message));
+            }
+        }
     }
     return 0;
 }
